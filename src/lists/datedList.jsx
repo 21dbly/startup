07a74978edeb7ff25498044fe347156db46405e8
Donovan <21dbly@gmail.com>
2024-11-16 09:57:46 -0700

    deleted some comments

--- src/lists/datedList.jsx
+++ src/lists/datedList.jsx
@@ -15,11 +15,6 @@ export function DatedList() {
         if (datedListJSON) {
             setDatedList(JSON.parse(datedListJSON));
         }
-        // setDatedList([{date:'11/12/2024', title:'Do Dishes', details:'just do them', time:'10:00pm', key:'dishes'},
-        //     {date:'11/12/2024', title:'Do dishes again', time:"5:00pm", key:'3'},
-        //     {date:'11/13/2024', title:'Do dishes again', time:"5:00pm", key:'2'},
-        //     {date:'11/13/2024', title:'Do dishes again', time:"5:00pm", key:'4'}
-        // ]);
     }, []);
 
     if (datedList.length === 0) {
@@ -44,8 +39,6 @@ export function DatedList() {
             <h2 className="can-add" key="0">
                 Today
                 <NewTaskButton date={today_YMD} />
-                {/* <form action="/newtask" className="add-button"><button type="submit">+</button></form> */}
-                {/* change form to onClick and make button its own component*/}
             </h2>
             {datedTable}
         </section>
@@ -59,8 +52,6 @@ function BlankList( {today} ) {
         <h2 className="can-add">
             Today
             <NewTaskButton date={today} />
-            {/* <form action="/newtask" className="add-button"><button type="submit">+</button></form> */}
-            {/* change form to onClick and make it its own component*/}
         </h2>
         <p>Nothing to do, add a new task!</p>
     </section>
@@ -73,43 +64,12 @@ function SingleDateList({ date, list}) {
     <h3 className="can-add">
         {date}
         <NewTaskButton date={date} />
-        {/* <form action="/newtask" className="add-button"><button type="submit">+</button></form> */}
     </h3>
     <ul>
         {list.map((item, index) => (
             <Task task={item} key={item.id}/>
-            // <li key={item.id}>
-            //     <span className='task-title'>{item.title}</span>{" - "}
-            //     <span className='task-details'>{item.details}</span>{" - "}
-            //     <span className='task-time'>{item.time}</span>
-            // </li>
         ))}
     </ul>
     </div>
     );
 }
-
-{/* <section id="today" className="list">
-    <h2 className="can-add">
-        Today
-        <form action="/newtask" className="add-button"><button type="submit">+</button></form>
-    </h2>
-    <h3 className="can-add">
-        Mon, May 20
-        <form action="/newtask" className="add-button"><button type="submit">+</button></form>
-    </h3>
-    <ul>
-        <li>task one</li>
-        <li>task two <span>12:00pm</span></li>
-        <li>task due! <span>11:59pm</span></li>
-    </ul>
-    <h3 className="can-add">
-        Tues, May 21
-        <form action="/newtask" className="add-button"><button type="submit">+</button></form>
-    </h3>
-    <ul>
-        <li>task goes here <span>(time in span)</span></li>
-        <li>some tasks have no time <span></span></li>
-        <li>click on task to see more details <span></span></li>
-    </ul>
-</section> */}
--- src/lists/undatedList.jsx
+++ src/lists/undatedList.jsx
@@ -12,9 +12,6 @@ export function UndatedList() {
         if (undatedListJSON) {
             setUndatedList(JSON.parse(undatedListJSON));
         }
-        // setUndatedList([{title:'Do something', details:'just do it', key:'1'},
-        //     {title:'Make a video game', details:"fantasy 2d storybased somewhat systematic roguelike", key:'2'}
-        // ]);
     }, []);
 
     if (undatedList.length === 0) {
@@ -33,8 +30,6 @@ export function UndatedList() {
             <h2 className="can-add" key="0">
                 To Do
                 <NewTaskButton />
-                {/* <form action="/newtask" className="add-button"><button type="submit">+</button></form> */}
-                {/* change form to onClick and make it its own component*/}
             </h2>
             {undatedTable}
         </section>
@@ -48,27 +43,8 @@ function BlankList() {
         <h2 className="can-add">
             To Do
             <NewTaskButton />
-            {/* <form action="/newtask" className="add-button"><button type="submit">+</button></form> */}
-            {/* change form to onClick and make button its own component*/}
         </h2>
         <p>Nothing to do, add a new task!</p>
     </section>
     );
 }
-
-{/* <section id="todo" className="list">
-        <h2 className="can-add">
-            To Do
-            <form action="/newtask" className="add-button"><button type="submit">+</button></form>
-        </h2>
-        <ul>
-            <li>just a list</li>
-            <li>a list of things to do</li>
-            <li>eventually</li>
-            <li>do laundry</li>
-            <li>go to sleep</li>
-            <li>things like that</li>
-            <li>by the way these lists will be stored in a database</li>
-            <li>and they'll be updated in real time from different devices using websockets</li>
-        </ul>
-    </section> */}
