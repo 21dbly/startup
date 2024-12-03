1ab4e51f8688d21dbe899c21ff5ff49d3b6f7d51
Donovan <21dbly@gmail.com>
2024-12-03 10:57:43 -0700

    gets login image from 3rd party service

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,19 +3,30 @@ import './login.css';
 import {Unauthenticated} from './unauthenticated.jsx';
 import {Authenticated} from './authenticated.jsx';
 
+
 export function Login(props) {
   const [imageUrl, setImageUrl] = React.useState('');
 
   React.useEffect(() => {
-    setImageUrl('https://images.pexels.com/photos/1955134/pexels-photo-1955134.jpeg');
-  }, [])
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        const imageContainer = document.querySelector('#picture');
+        const width = imageContainer.offsetWidth;
+        const height = imageContainer.offsetHeight;
+        setImageUrl(`https://picsum.photos/id/${data[0].id}/${width}/${height}`);
+      })
+      .catch();
+  }, []);
 
-  // console.log(props.isAuthorized)
   return (
     <main>
       { props.isAuthorized ? <Authenticated onLogout={() => props.onAuthChange('', false)}/> : <Unauthenticated onLogin={(userName) => props.onAuthChange(userName, true)}/>}
 
-      <img src={imageUrl} alt='random landscape image' width="250" />
+      {/* I can't get the image proportions how I want, but at least the service is working.
+      I'll have to mess with css more at some point */}
+      <img id='picture' src={imageUrl} alt='random landscape image' width='800' height='400'/>
     </main>
   );
 }
